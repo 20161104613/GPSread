6fe169cbe0d84d869257f6b43f77e4fb6e840db8
20161104613 <1772569458@qq.com>
2017-06-22 11:51:29 +0800

    修改
    修改

--- GPSread/main.c
+++ GPSread/main.c
@@ -10,17 +10,36 @@
 
 int main()
 {
-    char a[65];
-    int j,i,t;;
-    FILE *fp1,*fp2;
-    fp1=fopen("//Users//a20161104613//Downloads//TRAC-master//GPS170510.log ","r");
-    fp2=fopen("//Users//a20161104613//Desktop//GPSread//read.txt","w");
+    char str1[70];
+    char str2[70];
+    char lat[8];
+    //char lon[8];
+    int i;
+    FILE *fp1;
+    //Users/a20161104613/Downloads/TRAC-master/GPS170510.log
+    fp1=fopen("//Users//a20161104613//Downloads//TRAC-master//GPS170510.log","r");
+    //fp2=fopen("//Users//a20161104613//Desktop//GPSread//read.txt","w");
     if(fp1==NULL)
     {
         printf("打开文件错误！\n");
     }
     else
     {
-        
+        fscanf(fp1,"%s%s",str1,str2);
+        printf("结果是：\n%s\n%s\n",str1,str2);
+        for(i=0;i<8;i++)
+            lat[i]=str1[i+16];
+        lat[8]='\0';
+        if(str1[18]='N')
+        {
+        printf("纬度：北纬%s\n",lat);
+        }
+    else
+    {
+        printf("纬度：南纬%s\n",lat);
     }
+        fclose(fp1);
+        return 0;
+    }
+    
 }
