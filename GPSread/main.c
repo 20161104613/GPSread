df742c6558bceb2a5496386ce7205ac98a8a64e3
20161104613 <1772569458@qq.com>
2017-07-03 09:43:40 +0800

    Last
    last

--- GPSread/main.c
+++ GPSread/main.c
@@ -24,9 +24,7 @@ int main(int argc, const char * argv[]) {
         printf("打开文件错误，要打开的文件可能不存在");
     }
     else
-    {//$GPRMC,013238,A,4047.533,N,11141.856,E,012.2,253.6,100517,,*1A
-        //$GPGGA,013238,4047.533,N,11141.856,E,1,05,,1108,M,,M,,*57
-        ///Users/s20161104611/Desktop/test1/maopao.numbers
+    {
         while(fscanf(fp2,"$GPRMC,%6s,%c,%8s,%c,%9s,%c,%lf,%lf,%6s,,%3s\n$GPGGA,%6s,%lf,%c,%lf,%c,%d,%2s,,%4s,M,,M,,%3s\n",time,&gps,latitude,&latitude1,longitude,&longitude1,&v,&course,year,s,Time,&Latitude,&Latitude1,&Longitude,&Longitude1,&Gps,moon,altitude,S)!=EOF)
         {
             if(gps=='A')
@@ -141,35 +139,7 @@ int main(int argc, const char * argv[]) {
         }
         
     }
-    
-    //fscanf(fp2,"$GPGGA,%6s,%lf,%c,%lf,%c,%d,%s,,%4s,M,,M,,%3s",Time,&Latitude,&Latitude1,&Longitude,&Longitude1,&Gps,moon,altitude,S);
-    /*while(fscanf(fp2,"%d",&n)!=EOF)
-     {
-     for(i=0;i<n;i++)
-     {
-     fscanf(fp2,"%d",&a[i]);
-     }
-     for(j=0; j<n-1; j++)
-     {
-     for(k=0; k<n-1-j; k++)
-     {
-     if(a[k]>a[k+1])
-     {
-     t=a[k];
-     a[k]=a[k+1];
-     a[k+1]=t;
-     }
-     }
-     }
-     for(k=0; k<i; k++)
-     {
-     printf("%d ",a[k]);
-     fprintf(fp1,"%d ",a[k]);
-     }
-     fprintf(fp1,"\n");
-     printf("\n");
-     
-     }*/
+   
     fclose(fp1);
     fclose(fp2);
     fclose(fp3);
